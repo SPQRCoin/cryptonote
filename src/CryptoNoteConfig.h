7284e8699c6530ef0e4c92647f0684afab5cbd8c
SPQR Coin <35838109+SPQRCoin@users.noreply.github.com>
2018-01-31 17:08:34 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "spqrcoin.com:8080",
-  "spqrcoin.com:8080",
+  "165.227.150.89:34914",
+  "67.207.78.90:34914",
 };
 
 struct CheckpointData {
